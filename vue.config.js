3e935605c963d4eb41fdd07ad627693dec2dbb0c
baronkoko <rafaienko.o@gmail.com>
2020-11-19 13:23:30 +0200

    add deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_PUBLIC_PATH : '/',
 	chainWebpack: config => {
 		config.plugin('html').tap(args => {
 			args[0].title = 'Vue Modal Trap Focus Example';
